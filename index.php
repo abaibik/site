15ff924385fa34822643b6e0b488f63ac1ad41e8
Aleksandra Baibik <abaibik@gmail.com>
2021-02-24 18:52:42 +0100

    index.html to index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYP html>
+<html>
+<head>
+	<meta charset=<"utf-8">
+	<title>Личный сайт студентки GeekBrains</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="content">
+	<div class="header">
+		<a href="index.html">Главная</a>
+		<a href="puzzle.html">Загадки</a>
+		<a href="guess.html">Угадайка</a>
+	</div>
+
+	<p><h2>Личный сайт студентки GeekBrains</h2></p>
+
+		<div class="center">
+		<img src="img/photo.jpg">
+			<div class="box_text">
+			<p><b>Всем привет, меня зовут <i>Александра Байбик</i>.</b></p>
+			<p>Это сайт, который входит в обучение на курсе от <a href="https://geekbrains.ru">GeekBrains.</a></p>
+			<p>Здесь вы найдёте несколько игр, которые я написала. Я совсем недавно начала программировать, но все игры работают! :)
+			   Удивительно, правда? Мне-то уж точно. Можете проверить сами!</p>
+
+			<p><a href="index.html">Главная</a>
+			<a href="puzzle.html">Загадки</a>
+			<a href="guess.html">Угадайка</a></p>
+			</div>
+		</div>
+</div>
+
+<div class="footer">
+	Copyright &copy; <?php echo ;?> Aleksandra Baibik
+</div>
+		
+</body>
+</html>
+	
